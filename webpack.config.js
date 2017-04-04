cc5d512f0b356374844b3e89e6794fde16cdcb64
Yannick Dalbin <yannick@idvive.com>
2017-04-04 22:11:39 +0200

    TP2b.4: Ajoutez l'option publicPath dans l'option output

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "app.min.js"
+    filename: "app.min.js",
+    publicPath: "/assets/"
   },
   module: {
     rules: [
