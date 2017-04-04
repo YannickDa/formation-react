c56f35f1ab2c566e5b504db2c3ee4578c580de90
Yannick Dalbin <yannick@idvive.com>
2017-04-04 23:23:24 +0200

    TP3.6: Ajoutez le component TodoAdd et TodoList dans TodoApp

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 
+import TodoList from "./TodoList"
+import TodoAdd from "./TodoAdd"
+
 export default class TodoApp extends Component {
   render() {
     return (
-      <div className="TodoApp" />
+      <div className="TodoApp">
+        <TodoList />
+        <TodoAdd />
+      </div>
     )
   }
 }
