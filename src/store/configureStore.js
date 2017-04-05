4aced7601d95c5ecdae4bf9c7f1e6d64dbcce908
Yannick <yannick@idvive.com>
2017-04-05 23:38:29 +0200

    TP4c.2: Ajoutez redux-thunk à l'application

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+
 import reducers from "../reducers"
 
 export default (initialState = {}) => {
-  return createStore(reducers, initialState)
+  return createStore(reducers, initialState, applyMiddleware(thunk))
 }
