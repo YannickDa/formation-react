cdeef9ff59499555d847f6e3c7a26205516e3418
Yannick <yannick@idvive.com>
2017-04-06 01:26:47 +0200

    TP5.1: Sur le serveur, importez le configureStore, le Provider de redux ainsi que le StaticRouter de react-router-dom puis les utiliser dans un middleware Koa

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,10 @@ import serve from "koa-static"
 import mount from "koa-mount"
 import send from "koa-send"
 
+import { StaticRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import configureStore from "store/configureStore"
+
 import indexHtml from "./index.html"
 
 const todos = [
@@ -48,6 +52,24 @@ router.put("/todo/completed", async ctx => {
 
 app.use(router.routes())
    .use(router.allowedMethods())
+
+app.use(async ctx => {
+  const location = ctx.request.url
+  const context = {}
+
+  const store = configureStore({
+    todo: {
+      todos
+    }
+  })
+
+  const component = (
+    <Provider store={store}>
+      <StaticRouter location={location} context={context}>
+      </StaticRouter>
+    </Provider>
+  )
+})
 
 app.use(async ctx => {
   await send(ctx, indexHtml)
