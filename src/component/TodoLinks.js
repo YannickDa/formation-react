79c6c0e6700a9d74a0dba77f1fa97e1360df964f
Yannick Dalbin <yannick@idvive.com>
2017-04-05 08:36:24 +0200

    TP4a.4: Ajoutez les liens "afficher : tous, actives, terminées" avec le component Link de react-router-dom

--- src/component/TodoLinks.js
+++ src/component/TodoLinks.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
 
 export default class TodoLinks extends Component {
   render() {
     return (
-      <div className="TodoLinks" />
+      <div className="TodoLinks">
+        Afficher : 
+        <span>
+          <Link to={`${this.props.match}`}>
+            Tous
+          </Link>
+        </span> | 
+        <span>
+          <Link to={`${this.props.match}/active`}>
+            Actives
+          </Link>
+        </span> | 
+        <span>
+          <Link to={`${this.props.match}/completed`}>
+            Terminées
+          </Link>
+        </span>
+      </div>
     )
   }
 }
